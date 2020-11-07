8006b2168a69c70556026ec88da7430ed158cbeb
freedomFu <174780597@qq.com>
2020-11-07 11:21:04 +0800

    Clion

---
+++ ClionProjects/cmake-build-debug/CMakeFiles/ClionProjects.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ClionProjects.dir/ArrayTest/ArraySort.c.o"
+  "CMakeFiles/ClionProjects.dir/main.cpp.o"
+  "ClionProjects"
+  "ClionProjects.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/ClionProjects.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
